1e62ac4992c92b438744a0e8c6746e33c1e844fc
Adan Moreno <morenoadan22@gmail.com>
2016-01-12 16:39:15 -0800

    Improve fragment paging performance by limiting off screen loading

--- app/src/main/java/com/moreno/imgurviewer/ImageDetailActivity.java
+++ app/src/main/java/com/moreno/imgurviewer/ImageDetailActivity.java
@@ -31,15 +31,15 @@ public class ImageDetailActivity extends FragmentActivity {
         }
 
         LargeImageAdapter pageAdapter = new LargeImageAdapter(getSupportFragmentManager(), fragmentList);
-        ViewPager suggestedPager = (ViewPager) findViewById(R.id.pager_gallery);
-        suggestedPager.setOffscreenPageLimit(pageAdapter.getCount());
-        suggestedPager.setAdapter(pageAdapter);
-        suggestedPager.setClipChildren(false);
+        ViewPager imagePager = (ViewPager) findViewById(R.id.pager_gallery);
+        imagePager.setOffscreenPageLimit(pageAdapter.getCount() / 4); //Load a quarter of the pages to improver performance
+        imagePager.setAdapter(pageAdapter);
+        imagePager.setClipChildren(false);
 
         GalleryItem currentItem = getIntent().getParcelableExtra(Common.GALLERY_ID);
         int position = items.indexOf(currentItem);
         if( position != -1 ) {
-            suggestedPager.setCurrentItem(position);
+            imagePager.setCurrentItem(position);
         }
     }
 
